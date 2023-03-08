410aec6c8fe707802637c0f91c337f2af368b5d4
Vítor Capparelli S. Faria <vcapparelli@riseup.net>
2023-03-08 12:42:39 -0300

    first commit

---
+++ fila_normal.py
@@ -0,0 +1,22 @@
+class FilaNormal:
+    codigo:int = 0
+    fila = []
+    clientesatendidos = []
+    senhatual:str = ""
+
+    def gerarsenhaatual(self)->None:
+        self.senhatual = f'NM{self.codigo}'
+
+    def resetafila(self)->None:
+        if self.codigo >= 100:
+            self.codigo=0
+        else:
+            self.codigo+=1
+    def atualizafila(self)->None:
+        self.resetafila()
+        self.gerarsenhaatual()
+        self.fila.append(self.senhatual)
+    def chamacliente(self, caixa:int)->str:
+        cliente_atual:str = self.fila.pop(0)
+        self.clientesatendidos.append(cliente_atual)
+        return(f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
---
+++ main.py
@@ -0,0 +1,18 @@
+from fila_normal import filanormal
+from fila_prioritaria import FilaPrioritaria
+
+
+# fila_teste = filanormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+# print(fila_teste.chamacliente(10))
+
+fila_teste_2 = FilaPrioritaria()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+print(fila_teste_2.chama_cliente(10))
+print(fila_teste_2.chama_cliente(1))
+print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
