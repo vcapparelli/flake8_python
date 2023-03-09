b90b80f76e990a5cc264f23d9af4a1527eda2e93
Vítor Capparelli S. Faria <vcapparelli@riseup.net>
2023-03-09 14:29:48 -0300

    criando mais tipagens

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 
 # fila_teste = filanormal()
@@ -9,10 +10,16 @@ from fila_prioritaria import FilaPrioritaria
 # print(fila_teste.chamacliente(5))
 # print(fila_teste.chamacliente(10))
 
-fila_teste_2 = FilaNormal()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-print(fila_teste_2.chama_cliente(1))
+# fila_teste_2 = FilaNormal()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.chama_cliente(1))
 # print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
