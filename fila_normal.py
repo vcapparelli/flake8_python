98eb648da74bb38a07c3761a7a2ff0bf935e97e5
Vítor Capparelli S. Faria <vcapparelli@riseup.net>
2023-03-09 13:05:40 -0300

    criando metodo template

--- fila_normal.py
+++ fila_normal.py
@@ -2,15 +2,10 @@ from fila_base import FilaBase
 
 
 class FilaNormal(FilaBase):
-    def gerarsenhaatual(self) -> None:
+    def gera_senha_atual(self) -> None:
         self.senhatual = f'NM{self.codigo}'
 
-    def atualizafila(self) -> None:
-        self.reseta_fila()
-        self.gerarsenhaatual()
-        self.fila.append(self.senhatual)
-
-    def chamacliente(self, caixa: int) -> str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from fila_prioritaria import FilaPrioritaria
 # print(fila_teste.chamacliente(5))
 # print(fila_teste.chamacliente(10))
 
-fila_teste_2 = FilaPrioritaria()
+fila_teste_2 = FilaNormal()
 fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 print(fila_teste_2.chama_cliente(10))
 print(fila_teste_2.chama_cliente(1))
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+# print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
